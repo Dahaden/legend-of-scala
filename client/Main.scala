e4395e04986021d649dbabae35675a58902a4453
Tony Young <tony@rfw.name>
2013-09-24 08:48:06 +1200

    add custom iloop

--- client/Main.scala
+++ client/Main.scala
@@ -3,7 +3,16 @@ import nz.org.sesa.los.client
 import nz.org.sesa.los.client.items
 import nz.org.sesa.los.client.util
 
+import scala.tools.nsc.Settings
+import scala.tools.nsc.interpreter.ILoop
+
 package object los {
+    private class LosILoop extends ILoop {
+        addThunk {
+            intp.quietImport("los._")
+        }
+    }
+
     val login = Adventurer.login(_)
 
     val Markers = util.Markers
@@ -12,4 +21,12 @@ package object los {
     type Tile = items.Map.Tile
     type Signal = items.Beacon.Signal
     val Signal = items.Beacon.Signal
+
+    def main(args: Array[String]) {
+        val settings = new Settings()
+        settings.embeddedDefaults[Adventurer]
+
+        val iloop = new LosILoop()
+        iloop.process(settings)
+    }
 }
