0ae0e15fa0d1617bde9e4f073655243b667c2c91
Tony Young <tony@rfw.name>
2013-09-25 08:14:43 +1200

    add images to resources folder

--- client/src/main/scala/nz/org/sesa/los/client/Images.scala
+++ client/src/main/scala/nz/org/sesa/los/client/Images.scala
@@ -3,8 +3,8 @@ package nz.org.sesa.los.client
 import scala.io.Source
 
 object Images {
-    val Beacon = Source.fromFile("images/beacon.txt").mkString
-    val Map = Source.fromFile("images/map.txt").mkString
-    val Paper = Source.fromFile("images/legend.txt").mkString
-    val LogoSplash = Source.fromFile("images/splash.txt").mkString
+    val Beacon = Source.fromInputStream(this.getClass.getResourceAsStream("/images/beacon.txt")).mkString
+    val Map = Source.fromInputStream(this.getClass.getResourceAsStream("/images/map.txt")).mkString
+    val Paper = Source.fromInputStream(this.getClass.getResourceAsStream("/images/legend.txt")).mkString
+    val LogoSplash = Source.fromInputStream(this.getClass.getResourceAsStream("/images/splash.txt")).mkString
 }
