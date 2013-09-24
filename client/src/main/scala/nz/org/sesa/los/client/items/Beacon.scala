172cc0a7ed7d2f33b3883fcb2bea5d53340f6c08
Tony Young <tony@rfw.name>
2013-09-24 16:28:03 +1200

    move images to their own file

--- client/src/main/scala/nz/org/sesa/los/client/items/Beacon.scala
+++ client/src/main/scala/nz/org/sesa/los/client/items/Beacon.scala
@@ -3,6 +3,7 @@ package nz.org.sesa.los.client.items
 import nz.org.sesa.los.client._
 import nz.org.sesa.los.client.util._
 import nz.org.sesa.los.client.Item
+import nz.org.sesa.los.client.Images
 
 import dispatch._, Defaults._
 import net.liftweb.json
@@ -24,17 +25,8 @@ object Beacon {
 class Beacon(val id : Int, val attrs : json.JObject, val owner : Adventurer) extends Item {
     def name = "beacon"
     def examine = "It's some kind of glowing gem with weird glyphs on it. You can use it to find things on the map."
-    def image = s"""
-|    ${27.toChar}[1m${27.toChar}[48;5;31m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;31m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;31m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;31m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;25m  ${27.toChar}[0m
-|  ${27.toChar}[1m${27.toChar}[48;5;31m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;15m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;15m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;195m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;195m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;81m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;25m  ${27.toChar}[0m
-|${27.toChar}[1m${27.toChar}[48;5;31m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;15m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;81m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;38m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;38m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;31m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;31m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;117m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;25m  ${27.toChar}[0m
-|${27.toChar}[1m${27.toChar}[48;5;31m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;67m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;255m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;255m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;15m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;15m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;123m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;31m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;25m  ${27.toChar}[0m
-|${27.toChar}[1m${27.toChar}[48;5;31m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;81m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;31m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;81m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;123m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;81m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;31m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;117m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;25m  ${27.toChar}[0m
-|  ${27.toChar}[1m${27.toChar}[48;5;25m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;80m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;31m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;38m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;31m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;195m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;25m  ${27.toChar}[0m
-|    ${27.toChar}[1m${27.toChar}[48;5;25m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;74m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;24m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;195m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;25m  ${27.toChar}[0m
-|      ${27.toChar}[1m${27.toChar}[48;5;25m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;81m  ${27.toChar}[0m${27.toChar}[1m${27.toChar}[48;5;25m  ${27.toChar}[0m
-|        ${27.toChar}[1m${27.toChar}[48;5;25m  ${27.toChar}[0m
-""".trim.stripMargin
+    def image = Images.Beacon
+
     def action[T : Manifest](args: Any*) = () match {
         case _ if manifest[T] != manifest[List[Beacon.Signal]] => {
             Display.show("It looks like you can use the beacon to find a List of Signals.")
