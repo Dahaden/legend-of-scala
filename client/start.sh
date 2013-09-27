3dc053e4c20a07c573bb6ea900c3252b84f02c5e
Tony Young <tony@rfw.name>
2013-09-27 14:32:18 +1200

    add sbt launcher jar

--- client/start.sh
+++ client/start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 trap '' SIGINT
-export SBT_OPTS="-Xmx2G -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2G -Xss2M -Dfile.encoding=UTF-8"
-sbt run
+SBT_OPTS="-Xmx2G -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2G -Xss2M -Dfile.encoding=UTF-8" java -jar ./sbt-launch.jar run
